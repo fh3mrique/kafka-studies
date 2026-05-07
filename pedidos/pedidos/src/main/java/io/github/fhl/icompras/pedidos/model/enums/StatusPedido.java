dffc6a46d825dd71e2127c34d06ae4271a490d75
fh3mrique <fhemrique55@gmail.com>
2026-05-07 10:54:38 -0300

    Mapeamento JPA de Pedido e ItemPedido

---
+++ pedidos/pedidos/src/main/java/io/github/fhl/icompras/pedidos/model/enums/StatusPedido.java
@@ -0,0 +1,11 @@
+package io.github.fhl.icompras.pedidos.model.enums;
+
+public enum StatusPedido {
+    REALIZADO,
+    PAGO,
+    FATURADO,
+    ENVIADO,
+    ERRO_PAGAMENTO,
+    PREPARANDO_ENVIO
+
+    }
