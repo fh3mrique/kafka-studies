84f96f047d7d3659b85ad43b6bf3ea3f95ff93c8
fh3mrique <fhemrique55@gmail.com>
2026-05-18 16:23:59 -0300

    Lógica para persistir e solicitar pagamento do pedido

--- pedidos/pedidos/src/main/java/io/github/fhl/icompras/pedidos/client/ServicoBancarioClient.java
+++ pedidos/pedidos/src/main/java/io/github/fhl/icompras/pedidos/client/ServicoBancarioClient.java
@@ -1,4 +1,18 @@
 package io.github.fhl.icompras.pedidos.client;
 
+import io.github.fhl.icompras.pedidos.model.Pedido;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+//simula um gateway de pagamento
+@Component
+@Slf4j
 public class ServicoBancarioClient {
+
+    public String solicitarPagamento(Pedido pedido){
+        log.info("Solicitando pagamento para o pedido de código {}.", pedido.getCodigo());
+        return UUID.randomUUID().toString();
+    }
 }
--- pedidos/pedidos/src/main/java/io/github/fhl/icompras/pedidos/sevice/PedidoService.java
+++ pedidos/pedidos/src/main/java/io/github/fhl/icompras/pedidos/sevice/PedidoService.java
@@ -1,11 +1,13 @@
 package io.github.fhl.icompras.pedidos.sevice;
 
+import io.github.fhl.icompras.pedidos.client.ServicoBancarioClient;
 import io.github.fhl.icompras.pedidos.model.Pedido;
 import io.github.fhl.icompras.pedidos.repository.ItemPedidoRepository;
 import io.github.fhl.icompras.pedidos.repository.PedidoRepository;
 import io.github.fhl.icompras.pedidos.validator.PedidoValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,11 +16,27 @@ public class PedidoService {
     private final PedidoRepository repository;
     private final ItemPedidoRepository itemPedidoRepository;
     private final PedidoValidator validator;
+    private final ServicoBancarioClient servicoBancarioClient;
 
+    @Transactional()
     public Pedido criarPedido(Pedido pedido){
+        validator.validar(pedido);
+
+        realizarPersistencia(pedido);
+
+        enviarSolicitacaoPagamento(pedido);
+
+        return pedido;
+    }
+
+    private void enviarSolicitacaoPagamento(Pedido pedido) {
+        String chavePagamento = servicoBancarioClient.solicitarPagamento(pedido);
+        pedido.setChavePagamento(chavePagamento);
+    }
+
+    private void realizarPersistencia(Pedido pedido) {
         repository.save(pedido);
         itemPedidoRepository.saveAll(pedido.getItens());
-        return pedido;
     }
 
 
